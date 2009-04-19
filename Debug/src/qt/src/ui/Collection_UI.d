39269117f3352674d9db6a075eaf4db0c1d5a45c
oivasyuv <>
2009-04-20 00:12:01 +0300

    - remove unnided files

--- Debug/src/qt/src/ui/Collection_UI.d
+++
@@ -1,7 +0,0 @@
-src/qt/src/ui/Collection_UI.d src/qt/src/ui/Collection_UI.o:  \
- ../src/qt/src/ui/Collection_UI.cpp ../src/qt/src/ui/Collection_UI.hpp \
-  ../src/qt/src/ui/../LoadUI.hpp
-
-../src/qt/src/ui/Collection_UI.hpp:
-
-../src/qt/src/ui/../LoadUI.hpp:
